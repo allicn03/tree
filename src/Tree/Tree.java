3a4d4f5485ee2eaf3a8645d178fc7e2a02afc61c
allicn03 <a.neusch@gmail.com>
2021-05-03 15:44:27 -0400

    little java program to greet user and character draw a tree

---
+++ src/Tree/Tree.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class Tree {
+    public static void main(String[] args) {
+        //This method will introduce yourself to the user and plant a tree
+        System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
+        System.out.println("I'm going to plant a tree today!\n");
+        System.out.println("   *  ");
+        System.out.println("  *** ");
+        System.out.println(" ***** ");
+        System.out.println("   *  ");
+        System.out.println("   *  ");
+    }
+}
